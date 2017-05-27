4a98c26d9d508fac3a89348f04f5f6ff5e929140
Sebastián Toro <sebastian.brawl@hotmail.com>
2017-05-27 02:49:42 -0400

    Comenzadas los formularios, faltan métodos

---
+++ app/form.py
@@ -0,0 +1,23 @@
+from django import forms
+
+from .models import VendedorAmbulante
+from .models import VendedorFijo
+from .models import Alumno
+
+class VendedorAmbulanteForm(forms.ModelForm):
+
+    class Meta:
+        model = VendedorAmbulante
+        fields = ('email', 'password',)
+
+class VendedorFijoForm(forms.ModelForm):
+
+    class Meta:
+        model = VendedorFijo
+        fields = ('email', 'password',)
+
+class AlumnoForm(forms.ModelForm):
+
+    class Meta:
+        model = Alumno
+        fields = ('email', 'password',)
